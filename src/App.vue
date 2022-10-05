a45ae58178d38cddf23b9e655f826dea23ebd893
Edward NoaLand <edwardnoaland@gmail.com>
2022-10-05 17:14:53 +0800

    feat: implement call backend api

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,62 @@ export default {
         {id: 'property-insurance', name: '财产险产品'},
         {id: 'health-insurance', name: '健康险产品'}
       ],
-      selectedProduct: {id: '', name: ''}
+      selectedProduct: {id: '', name: ''},
+      inputParam: null
+    }
+  },
+  methods: {
+    submit() {
+      console.log("product", this.selectedProduct)
+      console.log("input param", this.inputParam)
+      let data;
+      switch (this.selectedProduct.id) {
+        case "dread-disease-insurance":
+          data = {
+            gender: this.inputParam
+          }
+          break
+        case "property-insurance":
+          data = {
+            value: this.inputParam
+          }
+          break
+        case "health-insurance":
+          data = {
+            age: this.inputParam
+          }
+          break
+        default:
+          alert("wrong type of product")
+      }
+      fetch('/api/' + this.selectedProduct.id, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+          .then((data) => {
+            if(data.status !== 200) {
+              data.text().then(error => alert("异常: " + error))
+            } else {
+              data.json().then(data => alert("保费：" + data.totalAmount))
+            }
+          })
     }
   }
 }
 </script>
 
 <template>
+  <h3>产品列表</h3>
   <select v-model="selectedProduct">
-    <option v-for="product in products" :value="product"> {{product.name}} </option>
+    <option v-for="product in products" :value="product"> {{ product.name }}</option>
   </select>
-  <h3 v-if="selectedProduct.id === 'dread-disease-insurance'">请输入性别</h3>
-  <h3 v-if="selectedProduct.id === 'property-insurance'">请输入房价</h3>
-  <h3 v-if="selectedProduct.id === 'health-insurance'">请输入年龄</h3>
+  <div v-if="selectedProduct.id === 'dread-disease-insurance'">请输入性别：<input v-model="inputParam"></div>
+  <div v-if="selectedProduct.id === 'property-insurance'">请输入房价：<input v-model="inputParam"></div>
+  <div v-if="selectedProduct.id === 'health-insurance'">请输入年龄：<input v-model="inputParam"></div>
+  <button @click="submit">提交</button>
 </template>
 
 <style>
