4daebfe8fdb31fac47510af40d2b4a8c08788f61
Edward NoaLand <edwardnoaland@gmail.com>
2022-10-05 15:58:46 +0800

    feat: will show info when select an option in product list

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,23 @@ export default {
   data() {
     return {
       products: [
-        {name: "重疾险产品"},
-        {name: "财产险产品"},
-        {name: "健康险产品"}
-      ]
+        {id: 'dread-disease-insurance', name: '重疾险产品'},
+        {id: 'property-insurance', name: '财产险产品'},
+        {id: 'health-insurance', name: '健康险产品'}
+      ],
+      selectedProduct: {id: '', name: ''}
     }
-  },
-  computed: {
-    // ...
-  },
-  methods: {
   }
 }
 </script>
 
 <template>
-  <select>
-    <option v-for="product in products"> {{product.name}} </option>
+  <select v-model="selectedProduct">
+    <option v-for="product in products" :value="product"> {{product.name}} </option>
   </select>
+  <h3 v-if="selectedProduct.id === 'dread-disease-insurance'">请输入性别</h3>
+  <h3 v-if="selectedProduct.id === 'property-insurance'">请输入房价</h3>
+  <h3 v-if="selectedProduct.id === 'health-insurance'">请输入年龄</h3>
 </template>
 
 <style>
